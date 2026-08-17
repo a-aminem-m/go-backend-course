6c556bcbef1247ec43fe01ee23ba558910570cd3
Амина Юнисова <amina.yunisova@gmail.com>
2026-08-17 14:09:09 +0300

    Handle connection write errors

--- hw0/server.go
+++ hw0/server.go
@@ -8,22 +8,27 @@ import (
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		fmt.Println("Ошибка:", err)
+		fmt.Println("Error:", err)
 		return
 	}
 
 	defer listener.Close()
 
-	fmt.Println("Сервер запущен")
+	fmt.Println("Run server")
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("Ошибка подключения:", err)
+			fmt.Println("Connection failed:", err)
 			continue
 		}
 
-		conn.Write([]byte("OK\n"))
+		_, err := conn.Write([]byte("OK\n"))
+		if err != nil {
+			fmt.Println("Write failed:", err)
+			conn.Close()
+			continue
+		}
 		conn.Close()
 	}
 }
