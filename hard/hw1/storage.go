0935a8275953c87abc796320c65ff962da23868a
Амина Юнисова <amina.yunisova@gmail.com>
2026-09-03 20:37:50 +0200

    Add memory storage

---
+++ hard/hw1/storage.go
@@ -0,0 +1,24 @@
+package main
+
+type MemoryStorage struct {
+	tasks map[string]Task
+}
+
+func NewMemoryStorage() *MemoryStorage {
+	storage := MemoryStorage{}
+	storage.tasks = make(map[string]Task)
+	return &storage
+}
+
+func (s *MemoryStorage) Create(task Task) {
+	s.tasks[task.ID] = task
+}
+
+func (s *MemoryStorage) Get(id string) (Task, bool) {
+	task, ok := s.tasks[id]
+	return task, ok
+}
+
+func (s *MemoryStorage) Update(task Task) {
+	s.tasks[task.ID] = task
+}
