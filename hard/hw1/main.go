6baee3ff4a51a5ea31f15370934cd52905c777b1
Амина Юнисова <amina.yunisova@gmail.com>
2026-09-06 01:38:59 +0200

    Register task result handler

--- hard/hw1/main.go
+++ hard/hw1/main.go
@@ -8,5 +8,6 @@ func main() {
 	storage := NewMemoryStorage()
 	http.HandleFunc("/task", createTaskHandler(storage))
 	http.HandleFunc("/status/", getTaskStatusHandler(storage))
+	http.HandleFunc("/result/", getTaskResultHandler(storage))
 	http.ListenAndServe(":8000", nil)
 }
