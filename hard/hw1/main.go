1cbba30b31fa676f8293ff80497ea933d147b810
Амина Юнисова <amina.yunisova@gmail.com>
2026-09-06 22:44:39 +0200

    Register user registration handler

--- hard/hw1/main.go
+++ hard/hw1/main.go
@@ -9,5 +9,6 @@ func main() {
 	http.HandleFunc("/task", createTaskHandler(storage))
 	http.HandleFunc("/status/", getTaskStatusHandler(storage))
 	http.HandleFunc("/result/", getTaskResultHandler(storage))
+	http.HandleFunc("/register", registerHandler(storage))
 	http.ListenAndServe(":8000", nil)
 }
