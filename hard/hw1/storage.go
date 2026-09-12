3607701378b50b4cfe325ca1ddb37c1b8b91f4be
Амина Юнисова <amina.yunisova@gmail.com>
2026-09-12 18:43:49 +0200

    Add mutexes for memory storage

--- hard/hw1/storage.go
+++ hard/hw1/storage.go
@@ -3,10 +3,12 @@ package main
 import "sync"
 
 type MemoryStorage struct {
-	tasks    map[string]Task
-	users    map[string]User
-	sessions map[string]Session
-	mu       sync.RWMutex
+	tasks      map[string]Task
+	users      map[string]User
+	sessions   map[string]Session
+	tasksMu    sync.RWMutex
+	usersMu    sync.RWMutex
+	sessionsMu sync.RWMutex
 }
 
 func NewMemoryStorage() *MemoryStorage {
@@ -18,40 +20,46 @@ func NewMemoryStorage() *MemoryStorage {
 }
 
 func (s *MemoryStorage) CreateTask(task Task) {
+	s.tasksMu.RLock()
+	defer s.tasksMu.Unlock()
 	s.tasks[task.ID] = task
 }
 
 func (s *MemoryStorage) GetTask(id string) (Task, bool) {
+	s.tasksMu.RLock()
+	defer s.tasksMu.RUnlock()
 	task, ok := s.tasks[id]
 	return task, ok
 }
 
 func (s *MemoryStorage) UpdateTask(task Task) {
+	s.tasksMu.Lock()
+	defer s.tasksMu.Unlock()
 	s.tasks[task.ID] = task
 }
 
 func (s *MemoryStorage) CreateUser(user User) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.usersMu.Lock()
+	defer s.usersMu.Unlock()
 	s.users[user.Login] = user
 }
 
 func (s *MemoryStorage) GetUser(login string) (User, bool) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
+	s.usersMu.RLock()
+	defer s.usersMu.RUnlock()
 	user, ok := s.users[login]
 	return user, ok
 }
 
 func (s *MemoryStorage) CreateSession(session Session) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
+	s.sessionsMu.Lock()
+	defer s.sessionsMu.Unlock()
 	s.sessions[session.SessionID] = session
 }
 
 func (s *MemoryStorage) GetSession(sessionID string) (Session, bool) {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
+	s.sessionsMu.RLock()
+	defer s.sessionsMu.RUnlock()
 	session, ok := s.sessions[sessionID]
 	return session, ok
 }
